07a23d1ecfe3e1f506dba164052958f9d6927f60
cedric.bingen <cedric.bingen@OntCur-0E180363.vdabheverlee.intra>
2018-07-02 08:12:33 +0200

    eerste commit bierhuis springboot

---
+++ src/main/java/be/vdab/bierhuis/BierhuisApplication.java
@@ -0,0 +1,12 @@
+package be.vdab.bierhuis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BierhuisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BierhuisApplication.class, args);
+	}
+}
